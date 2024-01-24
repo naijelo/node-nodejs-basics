209059591367ba46db2e24cdf509479a1da09641
naijelo <naijel.a@gmail.com>
2024-01-24 22:58:24 +0300

    feat: add implementation fs-create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
-const create = async () => {
-    // Write your code here 
-};
+import fs from "fs";
 
+const data = "I am fresh and young";
+const errMsg = "FS operation failed";
+const path = './src/fs/files/fresh.txt'
+
+const create = async () => {
+    fs.access(path, fs.constants.F_OK, (err) => {
+        if (err) {
+            fs.writeFile(path, data, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        } else {
+            console.log(errMsg);
+        }
+    });
+}
 await create();
