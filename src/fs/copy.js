0358483f657443f78a384327f33185d51970b881
naijelo <naijel.a@gmail.com>
2024-01-26 00:31:55 +0300

    feat: add implementation  fs-copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import fs from "fs";
+import { copyFile } from 'node:fs/promises';
+
+const pathToFiles = "./src/fs/files";
+const pathToCopy = "./src/fs/files_copy";
+const errMsg = "FS operation failed";
+
 const copy = async () => {
-    // Write your code here 
+    await fs.readdir(pathToFiles, {withFileTypes: true}, async (err, files) => {
+        if (err) {
+            console.log(errMsg);
+            return;
+        }
+        fs.mkdir(pathToCopy, (err) => {
+            if (err) {
+                console.log(errMsg);
+            } else {
+                files.forEach((file) => {
+                    if(!file.isDirectory()) {
+                        copyFile(`${pathToFiles}/${file.name}`, `${pathToCopy}/${file.name}`);
+                    }
+                });
+            }
+        });
+    });
 };
 
 await copy();
