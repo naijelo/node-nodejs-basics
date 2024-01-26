825689057db6adf6a5b5d5a882ba787eeb58f279
naijelo <naijel.a@gmail.com>
2024-01-26 21:24:10 +0300

    feat: add fs-rename task implementation

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import { copyFile } from 'node:fs/promises';
+
+const pathToWrong = "./src/fs/files/wrongFilename.txt";
+const errMsg = "FS operation failed";
+
 const rename = async () => {
-    // Write your code here 
+    fs.access(pathToWrong, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.log(errMsg);
+        } else {
+            fs.promises.rename(pathToWrong, "./src/fs/files/properFilename.md");
+        }
+    });
 };
 
 await rename();
