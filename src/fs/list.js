419e3aea3049f5d8c1acbd68364f180945f2b526
naijelo <naijel.a@gmail.com>
2024-01-26 23:00:30 +0300

    feat: add task fs-read implementation

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+
+const errMsg = "FS operation failed";
+const pathToFiles = "./src/fs/files/";
+
+
 const list = async () => {
-    // Write your code here 
+    fs.readdir(pathToFiles, {withFileTypes: true}, (err, files) => {
+        if (err)
+          console.log(errMsg);
+        else { 
+          files.forEach(file => {
+            if(!file.isDirectory()) {
+              (async () => {
+                  await fs.promises.stat((`${pathToFiles}/${file.name}`));
+                  console.log(file.name);
+              })();
+            }
+          });
+        };
+    });
 };
 
 await list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+
+const errMsg = "FS operation failed";
+const pathToRead = "./src/fs/files/fileToRead.txt";
+
 const read = async () => {
-    // Write your code here 
+    fs.access(pathToRead, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.log(errMsg);
+        } else {
+            const readStream = fs.createReadStream(pathToRead);
+            readStream.on("data", (chunk) => {
+            console.log(chunk.toString());
+        });
+        }
+    });
 };
 
 await read();
