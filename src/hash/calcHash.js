e908b89d4113223221eef2399f1b8136a1abce93
naijelo <naijel.a@gmail.com>
2024-01-29 17:43:20 +0300

    feat: add hash task implementation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    const dirPath = dirname(fileURLToPath(import.meta.url));
+    const pathToFile = path.join(dirPath, "/files/fileToCalculateHashFor.txt");
+    const readStream = createReadStream(pathToFile);
+    const hash = createHash("sha256", readStream)
+        .digest("hex");
+    console.log(hash);
 };
 
 await calculateHash();
