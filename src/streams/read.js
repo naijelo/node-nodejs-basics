791b3081bcf8a034851a0ea622eba72aaebc2e68
naijelo <naijel.a@gmail.com>
2024-01-29 18:07:16 +0300

    feat: add streams-read task implementation

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+
+const dirPath = dirname(fileURLToPath(import.meta.url));
+const pathToFile = path.join(dirPath, "/files/fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(pathToFile);
+    readStream.on("data", (chunk) => {
+        process.stdout.write(chunk);
+    })
 };
 
 await read();
