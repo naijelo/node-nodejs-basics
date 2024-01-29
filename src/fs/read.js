a3bedb71bb9080c0d1eba14d4894c4c21d0a7f65
naijelo <naijel.a@gmail.com>
2024-01-29 13:47:10 +0300

    fix: issues with path in fs-read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,7 +1,10 @@
 import fs from "fs";
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
 
+const dirPath = dirname(fileURLToPath(import.meta.url));
+const pathToRead = path.join(dirPath, "/files/fileToRead.txt");
 const errMsg = "FS operation failed";
-const pathToRead = "./src/fs/files/fileToRead.txt";
 
 const read = async () => {
     fs.access(pathToRead, fs.constants.F_OK, (err) => {
